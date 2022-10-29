e93ff2ea521229098968545094d1c2f59e587e8c
charbonierlouis <louis.charbonier@gmail.com>
2022-10-29 14:43:49 +0200

    refactor: export ControllerConfig, CotrollerType

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { ApiHandler } from './handlers';
 export { Service } from './services';
 export { Controller } from './controllers';
-export type { ServiceConfig } from './types';
+export type { ServiceConfig, ControllerConfig, CotrollerType } from './types';
